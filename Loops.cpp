c73a3b76e2c1ec4ee05799c1aa930e9342600282
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2023-11-21 20:59:59 +0100

    HW6 added Task 2, modified Task 1

--- Loops.cpp
+++ Loops.cpp
@@ -18,13 +18,16 @@ int main()
     std::cout << std::endl;
 
     if (
-        (arProgressionN - arProgression1) <= (arProgressionN - (arProgression1 + step)) || 
-        (arProgressionN - arProgression1) % step != 0
+        step == 0 || // No step -> infinite sequence
+        (arProgressionN - arProgression1) < (arProgressionN - (arProgression1 + step)) || // Progression goes in wrong direction
+        (arProgressionN - arProgression1) % step != 0 || // Last number does not belong to sequence
+        arProgression1 == arProgressionN // First number equals to last number -> no sequence
     ) {
         std::cout << "Entered elements don't create ariphmetic progression" << std::endl;
     }
     else {
         std::cout << "Ariphmetic progression: " << arProgression1 << " ";
+
         do {
             arProgression1 += step;
             std::cout << arProgression1 << " ";
@@ -32,5 +35,31 @@ int main()
     }
     
     std::cout << std::endl << std::endl;
+
+    // ---------- TASK 2 ----------
+    std::cout << "---------- TASK 2 ----------" << std::endl << std::endl;
+
+    unsigned int fibonacciNumber;
+
+    std::cout << "Enter number of the element of the Fibonacci sequence: ";
+    std::cin >> fibonacciNumber;
+    std::cout << std::endl;
+
+    if (fibonacciNumber == 1 || fibonacciNumber == 2) {
+        std::cout << "Fibonacci sequence element: " << 1 << std::endl << std::endl;
+    }
+    else {
+        int element1 = 1;
+        int element2 = 1;
+        int swap;
+
+        for (int i = 3; i <= fibonacciNumber; i++) {
+            swap = element2;
+            element2 = element2 + element1;
+            element1 = swap;
+        }
+
+        std::cout << "Fibonacci sequence element: " << element2 << std::endl << std::endl;
+    }
 }
 
