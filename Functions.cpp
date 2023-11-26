1e134d3eaf8954d74b723ea6f81b178266e7bd79
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2023-11-26 11:55:01 +0100

    HW8 Task3

--- Functions.cpp
+++ Functions.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-int factorialRec(unsigned int count);
+void printNaturalNumbersA(int count);
+void printNaturalNumbersB(int count);
+void printNaturalNumbersC(int count);
+void printNaturalNumbersD(int count);
+void helpFunctionD(int count, int);
 
+int factorialRec(unsigned int count);
 int factorialLoop(unsigned int count);
 
 void countBySign(int n);
@@ -33,6 +38,23 @@ int main()
 	int factorialB = factorialLoop(numberFactorial);
 	std::cout << "Factorial based on loops equals to " << factorialB << ".";
 	std::cout << std::endl << std::endl;
+
+	// -------------- TASK 3 --------------
+	std::cout << "-------------- TASK 3 --------------" << std::endl << std::endl;
+
+	int naturalNumbers = 0;
+	std::cout << "Enter a number of first natural numbers to show: "; 
+	std::cin >> naturalNumbers;
+	std::cout << std::endl;
+
+	std::cout << "First " << naturalNumbers << " natural numbers in descending order using loops: " << std::endl;
+	printNaturalNumbersA(naturalNumbers); std::cout << std::endl;
+	std::cout << "First " << naturalNumbers << " natural numbers in ascending order using loops: " << std::endl;
+	printNaturalNumbersB(naturalNumbers); std::cout << std::endl;
+	std::cout << "First " << naturalNumbers << " natural numbers in descending order using recursion: " << std::endl;
+	printNaturalNumbersC(naturalNumbers); std::cout << std::endl;
+	std::cout << "First " << naturalNumbers << " natural numbers in ascending order using recursion: " << std::endl;
+	printNaturalNumbersD(naturalNumbers); std::cout << std::endl;
 }
 
 int factorialRec(unsigned int count) {
@@ -80,4 +102,45 @@ void countBySign(int n) {
 
 	std::cout << std::endl;
 	std::cout << "You entered " << zeros << " zeros, " << positives << " positive and " << negatives << " negative numbers." << std::endl;
+}
+
+void printNaturalNumbersA(int count) {
+	while (count > 0) {
+		std::cout << count << " ";
+		count--;
+	}
+	std::cout << std::endl;
+}
+void printNaturalNumbersB(int count) {
+	int i = 1; 
+
+	while (i <= count) {
+		std::cout << i << " ";
+		i++;
+	}
+
+	std::cout << std::endl;
+}
+void printNaturalNumbersC(int count) {
+	if (count == 1) {
+		std::cout << count << std::endl;
+	}
+	else {
+		std::cout << count << " ";
+		printNaturalNumbersC(count - 1);
+	}
+}
+
+void helpFunctionD(int count, int i = 1) {
+	if (i == count) {
+		std::cout << i << std::endl;
+	}
+	else {
+		std::cout << i << " ";
+		helpFunctionD(count, i + 1);
+	}
+}
+
+void printNaturalNumbersD(int count) {
+	helpFunctionD(count);
 }
