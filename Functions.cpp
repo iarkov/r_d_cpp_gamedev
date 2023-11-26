1baf31215fa49208ebe4f967450c04a324a589a1
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2023-11-26 11:02:30 +0100

    HW8 Task 2

--- Functions.cpp
+++ Functions.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 
+int factorialRec(unsigned int count);
+
+int factorialLoop(unsigned int count);
+
+void countBySign(int n);
+
+
+int main()
+{
+	// -------------- TASK 1 --------------
+	std::cout << "-------------- TASK 1 --------------" << std::endl << std::endl;
+
+	int numberCBS = 0;
+	std::cout << "Enter number for function coutnBySigh(): "; 
+	std::cin >> numberCBS;
+	countBySign(numberCBS);
+
+	std::cout << std::endl;
+
+	// -------------- TASK 2 --------------
+	std::cout << "-------------- TASK 2 --------------" << std::endl << std::endl;
+
+	int numberFactorial = 0;
+	std::cout << "Enter number for function factorial(): ";
+	std::cin >> numberFactorial;
+
+	int factorialA = factorialRec(numberFactorial);
+	std::cout << "Factorial based on recursion equals to " << factorialA << ".";
+	std::cout << std::endl;
+
+	int factorialB = factorialLoop(numberFactorial);
+	std::cout << "Factorial based on loops equals to " << factorialB << ".";
+	std::cout << std::endl << std::endl;
+}
+
+int factorialRec(unsigned int count) {
+	return (count == 0) ? 1 : count * factorialRec(count - 1);
+}
+
+int factorialLoop(unsigned int count) {
+
+	int factorial = 1;
+
+	if (count == 0) {
+		return factorial;
+	}
+	else {
+		while (count > 0) {
+			factorial = factorial * count;
+			count--;
+		}
+
+		return factorial;
+	}
+}
+
 void countBySign(int n) {
-	int number = 0; 
+	int number = 0;
 	int positives = 0;
 	int negatives = 0;
 	int zeros = 0;
@@ -24,18 +80,4 @@ void countBySign(int n) {
 
 	std::cout << std::endl;
 	std::cout << "You entered " << zeros << " zeros, " << positives << " positive and " << negatives << " negative numbers." << std::endl;
-}
-
-
-int main()
-{
-	// -------------- TASK 1 --------------
-	std::cout << "-------------- TASK 1 --------------" << std::endl << std::endl;
-
-	int numberCBS = 0;
-	std::cout << "Enter number for function coutnBySigh(): "; 
-	std::cin >> numberCBS;
-	countBySign(numberCBS);
-
-	std::cout << std::endl;
 }
