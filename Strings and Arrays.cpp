a437fcfc812b3036a31afb5de7591868cc70a601
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2023-11-29 16:49:07 +0100

    HW9 Task 1

--- Strings and Arrays.cpp
+++ Strings and Arrays.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
 #include <cctype>
 
+const int arraySizeTask1 = 10;
 const int arrayLength = 255;
 
+void translateArray(int numbers[], int size);
 void toUpperCase(char str[]);
 
 int main()
 {
+    // --------------- TASK 1 ---------------
+    std::cout << "--------------- TASK 1 ---------------" << std::endl << std::endl;
+    int numberArray[arraySizeTask1]{0};
+
+    std::cout << "Enter array of 10 numbers (with ' ' as separator): ";
+    for (int i = 0; i < arraySizeTask1; i++) {
+        std::cin >> numberArray[i];
+    }
+
+    translateArray(numberArray, arraySizeTask1);
+    std::cout << std::endl << std::endl;
+
     // --------------- TASK 2 ---------------
     std::cout << "--------------- TASK 2 ---------------" << std::endl << std::endl;
 
-    char str[arrayLength];
+    char charArrayTask2[arrayLength]{};
 
     std::cout << "Enter character array: ";
-    std::cin.getline(str, arrayLength);
+    std::cin >> charArrayTask2;
 
     std::cout << "Character array in upper case: " << std::endl;
-    toUpperCase(str);
+    toUpperCase(charArrayTask2);
     std::cout << std::endl;
+}
+
+void translateArray(int numbers[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] > 0) {
+            std::cout << numbers[i] * 2 << " ";
+        }
+        else {
+            std::cout << 0 << " ";
+        }
+    }
 }
 
 void toUpperCase(char str[]) {
