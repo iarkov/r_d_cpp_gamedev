5e3c2861f9c0113f8c9ed86dd1ce90af5817f039
Oleh Yarkovyi <oleh.yarkovyi@gmail.com>
2023-12-04 11:07:00 +0100

    HW10 Task 2

--- Pointers and References.cpp
+++ Pointers and References.cpp
@@ -4,6 +4,8 @@ void swap(int& a, int& b);
 
 void swap(int* a, int* b);
 
+bool calculateSum(const double* arr, int arrSize, double& sum);
+
 int main()
 {
     // ---------------- TASK 1 ----------------
@@ -19,6 +21,27 @@ int main()
 
     swap(&x, &y);
     std::cout << "X: " << x << ", Y: " << y << std::endl;
+
+    std::cout << std::endl;
+
+    // ---------------- TASK 2 ----------------
+    std::cout << "---------------- TASK 2 ----------------" << std::endl << std::endl;
+
+    double arrTrue[]{ 213.232, 3232.111, 764.05 };
+    double sumTrue = 0.; 
+
+    bool tru = calculateSum(arrTrue, sizeof(arrTrue) / sizeof(double), sumTrue);
+
+    std::cout << "Size of the array is greater then " << static_cast<int>(!tru);
+    std::cout << " and sum of its elements equals to " << sumTrue << std::endl << std::endl;
+
+    double arrFalse[100];
+    double sumFalse = 0.;
+
+    bool fal = calculateSum(arrFalse, -99, sumFalse);
+
+    std::cout << "Size of the array is less than or equals to " << static_cast<int>(fal);
+    std::cout << " and sum of its elements equals to " << sumFalse << std::endl << std::endl;
 }
 
 void swap(int* a, int* b) {
@@ -35,4 +58,17 @@ void swap(int& a, int& b) {
     a = temp;
 
     std::cout << "Values swapped" << std::endl;
+}
+
+bool calculateSum(const double* arr, int arrSize, double& sum) {
+    if (arrSize <= 0) {
+        return false;
+    }
+    else {
+        for (int i = 0; i < arrSize; i++) {
+            sum += arr[i];
+        }
+
+        return true;
+    }
 }
